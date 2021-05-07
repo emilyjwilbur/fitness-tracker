59e19162ed00022865bd1e9fc4735813a95bbc59
Emily Wilbur <ewilbur94@gmail.com>
2021-05-07 17:00:05 -0400

    routes,server modificiation, file modification, etc

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
 const express = require("express");
-// const morgan = require("morgan");
+const logger = require("morgan");
 const mongoose = require("mongoose");
-const path = require("path");
+
 
 
 const PORT = process.env.PORT || 3005;
 
 const app = express();
 
+app.use(logger('dev'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
-app.use(express.static(path.join(__dirname, "public")));
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fit", { 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
     useNewUrlParser: true,
-    useFindAndModify: false
+    useFindAndModify: false,
+    useUnifiedTopology: true 
 });
 
-app.use(require("./routes/api-routes"));
+// routes
+
+app.use(require("./routes/htmlRoutes"));
 
 
 
